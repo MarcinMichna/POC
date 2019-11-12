1c428d2aa8054e6f68e4d91534f9d1240ad8674e
MarcinMichna <marcinxmichna@gmail.com>
2019-11-12 18:29:27 +0100

    lab7 on labs

--- lab7/convolution.m
+++ lab7/convolution.m
@@ -1,8 +1,8 @@
-%function data_filtered = convolution(data, local_window)
+function data_filtered = convolution(data, local_window)
 
 FUNCTION = @(data_,local_window)convolution_local(data_,local_window);
 
-data_filtered = colfilt(datalocal_window, 'sliding', FUNCTION, local_window);
+data_filtered = colfilt(data,local_window, 'sliding', FUNCTION, local_window);
 
 
 
